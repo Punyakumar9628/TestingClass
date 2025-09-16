f2a4dd328adb0f6a145dee8c40230ffcab2eed64
TestingClass <punyakn9628@gmail.com>
2025-09-16 09:31:47 +0530

    Scripting

---
+++ Automation/src/Scripting3/testScript.java
@@ -0,0 +1,55 @@
+package Scripting3;
+
+import Scripting.ReusableMethods;
+import org.openqa.selenium.WebDriver;
+import java.util.Map;
+
+import static Scripting3.ReusableMethods.getPropData;
+
+public class testScript extends ReusableMethods {
+    public static void main(String[] args){
+        TS_LoginLogout();
+        TS_createAndDeleteUser();
+    }
+        public static void TS_LoginLogout() {
+            WebDriver oBrowser = null;
+            Map<String, String> data=null;
+            try {
+                data = getPropData("TestData");
+                oBrowser = launchBrowser(data.get("browserName"));
+                boolean blnRes = navigateURL(oBrowser, data.get("url"));
+                blnRes = loginToApplication(oBrowser, data.get("userName"), data.get("password"));
+                blnRes = logoutFromActiTime(oBrowser);
+
+            } catch (Exception e) {
+                System.out.println("Exception in 'TS_LoginLogout' test script");
+            } finally {
+                oBrowser.close();
+                oBrowser = null;
+            }
+        }
+    public static void TS_createAndDeleteUser() {
+        WebDriver oBrowser = null;
+        Map<String, String> data = null;
+        try {
+            data = getPropData("TestData");
+            oBrowser = launchBrowser(data.get("browserName"));
+            boolean blnRes = navigateURL(oBrowser, data.get("url"));
+            blnRes = loginToApplication(oBrowser, data.get("userName"), data.get("password"));
+            String create = createUser(oBrowser,data);
+            boolean delete = deleteUser(oBrowser, create);
+            blnRes = logoutFromActiTime(oBrowser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            oBrowser.close();
+            oBrowser = null;
+
+        }
+    }
+}
+
+
+
+
+
